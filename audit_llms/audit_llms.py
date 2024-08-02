6d5adb1243c5eb6a6b190cc231abac46dabd20f0
stephaniebrandl <brandl@di.ku.dk>
2024-08-02 15:44:13 +0200

    added auditing based on legislature, just include flag audit_chronos to run this, different output file is generated

--- audit_llms/audit_llms.py
+++ audit_llms/audit_llms.py
@@ -6,11 +6,12 @@ import tqdm
 import os
 from helpers import normalize_responses
 from configure_prompt import build_prompt
-from helpers import PROMPTS
+from helpers import PROMPTS as system_prompts
 from data import DATA_DIR
 from peft import PeftModel
 import argparse
 import json
+import sys
 
 
 def main():
@@ -23,7 +24,8 @@ def main():
     parser.add_argument('--party', default='S&D', help='Party name to consider when filtering')
     parser.add_argument('--repetition_penalty', default=1.0, type=float, help='Repetition penalty')
     parser.add_argument('--max_length', default=128, type=int, help='Maximum length of the generated text')
-    parser.add_argument('--debug', default=True, type=bool, help='Whether to use debug mode')
+    parser.add_argument('--debug', action=argparse.BooleanOptionalAction, help='Whether to use debug mode')
+    parser.add_argument('--audit_chronos', action=argparse.BooleanOptionalAction, help='Audit the model in periods of the EU legislative term')
     config = parser.parse_args()
 
     if config.debug:
@@ -34,6 +36,26 @@ def main():
         config.max_length = 8
     else:
         tokenizer_name = config.model_name
+
+    # Term	Time Period
+    # 7th	14/07/2009 - 17/04/2014
+    # 8th	01/07/2014 - 18/04/2019
+    # 9th	02/07/2019 - 15/07/2024
+    if config.audit_chronos:
+        print('Auditing model in periods of the EU legislative term')
+        PROMPTS = []
+        legislature_dict = {'7th': ('2009', '2014'),
+                            '8th': ('2014', '2019'),
+                            '9th': ('2019', '2024')}
+        prompts_order = []
+        for leg in legislature_dict.keys():
+            for idx, system_prompt in enumerate(system_prompts):
+                PROMPTS.append(system_prompt.replace(
+                    'in the European Parliament', 
+                    f'in the {leg} European Parliament from {legislature_dict[leg][0]} to {legislature_dict[leg][1]}'))
+                prompts_order.append(f'{leg}_{idx}')
+    else:
+        PROMPTS = system_prompts
 
     # Load EUANDI questionnaire dataset
     euandi_questionnaire = []
@@ -78,7 +100,7 @@ def main():
             token=True,
             max_memory=max_memory if not config.debug else None,
             torch_dtype=torch.float16 if torch.cuda.is_available() else None,
-            attn_implementation="flash_attention_2" if not config.debug else None,
+            # attn_implementation="flash_attention_2" if not config.debug else None,
         )
     else:
         print('Loading custom DAPT model locally..')
@@ -111,31 +133,40 @@ def main():
         print('INSTRUCTION:\n', example["annotation_request"])
         for idx, system_prompt in enumerate(PROMPTS):
             temp_prompt = system_prompt.format(config.party)
+            if config.audit_chronos:
+                print(f'EU Legislative Term: {temp_prompt}')
             annotation_request = tokenizer.apply_chat_template(conversation=[{"role": "system", "content": temp_prompt},
                                                                              {"role": "user", "content": example["annotation_request"]}],
-                                                               tokenize=False, add_generation_prompt=False)
+                                                               tokenize=False, add_generation_prompt=True)
 
             annotation_request += 'I am most aligned with option ('
-            try:
-                # Get the response from the chatbot
-                responses = pipeline(
-                    annotation_request,
-                    do_sample=True,
-                    num_return_sequences=1,
-                    return_full_text=False,
-                    max_new_tokens=config.max_length,
-                    eos_token_id=tokenizer.eos_token_id,
-                    bos_token_id=tokenizer.bos_token_id,
-                    repetition_penalty=config.repetition_penalty,
-                )
+            print(annotation_request)
+            # try:
+            # Get the response from the chatbot
+            responses = pipeline(
+                annotation_request,
+                do_sample=True,
+                num_return_sequences=1,
+                return_full_text=False,
+                max_new_tokens=config.max_length,
+                eos_token_id=tokenizer.eos_token_id,
+                bos_token_id=tokenizer.bos_token_id,
+                repetition_penalty=config.repetition_penalty,
+            )
 
-                # Print the response
-                print(f'RESPONSE GIVEN PROMPT [{idx}]:\nI am most aligned with option ({responses[0]["generated_text"].strip()}')
-                print("-" * 50)
-                # Save the response
-                example[f"response_{idx}"] = '(' + responses[0]['generated_text'].strip()
+            # Print the response
+            print(f'RESPONSE GIVEN PROMPT [{idx}]:\nI am most aligned with option ({responses[0]["generated_text"].strip()}')
+            print("-" * 50)
+            # Save the response
+            try:
+                if config.audit_chronos:
+                    example[f"response_{prompts_order[idx]}"] = '(' + responses[0]['generated_text'].strip()
+                else:
+                    example[f"response_{idx}"] = '(' + responses[0]['generated_text'].strip()
             except:
                 print('RESPONSE: None\n')
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print(f'Error: {exc_type} at line {exc_tb.tb_lineno}')
                 # Save the response
                 example[f"response_{idx}"] = 'N/A'
                 examples.append(example)
@@ -146,7 +177,13 @@ def main():
 
     # Normalize the responses
     for idx in range(len(PROMPTS)):
-        examples = normalize_responses(examples, idx, config.shuffle)
+        if config.audit_chronos:
+            examples = normalize_responses(examples, prompts_order[idx])
+        else:
+            examples = normalize_responses(examples, idx)
+
+    if config.audit_chronos:
+        output_name = f"{output_name}_chronos"
 
     # Save the responses to a jsonl file
     with open(os.path.join(DATA_DIR, "model_responses", f"{output_name}_responses.jsonl"), "w") as f:
