264063ccde0cee718afc65e5937286177a7fcc12
kiddothe2b <ilias.chalkidis@di.ku.dk>
2024-08-28 11:23:36 +0200

    Finetuning script

--- run_scripts/finetune_llms.sh
+++ run_scripts/finetune_llms.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=8 --mem=16000M
 #SBATCH -p gpu --gres=gpu:a100:1
-#SBATCH --output=/home/rwg642/eu-politics-llms-chronos/finetune_llama_3_sd_2014.txt
-#SBATCH --time=10:00:00
+#SBATCH --output=/home/rwg642/eu-politics-llms-chronos/finetune_llama_3_id_2019.txt
+#SBATCH --time=6:00:00
 
 . /etc/profile.d/modules.sh
 eval "$(conda shell.bash hook)"
